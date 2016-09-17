428bdd28eca0722a9f08116a3a468bfc85716332
Suvrath Penmetcha <spenmetc@andrew.cmu.edu>
2016-09-17 17:28:54 -0400

    merged changes from master branch

--- routes/mongoRoutes.js
+++ routes/mongoRoutes.js
@@ -1,19 +1,13 @@
 //include the mongo model
 var mongoModel = require('../models/mongoModel.js')
+var apiRoutes = require('apiRoutes.js')
 var nodemailer = require('nodemailer')
 var smtpTransport = require('nodemailer-smtp-transport');
 var crypto = require('crypto');
-var transporter = nodemailer.createTransport(smtpTransport({
-    service: 'gmail',
-    auth: {
-      user: 'cmufeedbackr@gmail.com',
-      pass: 'cmufeedbackr1'
-    }
-  }));
 
 exports.init = function(app) {
 
-  app.get('/', sendEmail)
+  app.get('/', apiRoutes.sendEmail)
 
   app.get('/mongo/:collection', doRead); // CRUD Retrieve
 
@@ -22,33 +16,6 @@ exports.init = function(app) {
   app.post('/mongo/:collection', doUpdate); // CRUD Update
 
   app.delete('/mongo/:collection', doDelete); //CRUD Delete
-
-}
-
-generateToken = function(s){
-  token = crypto.createHash('md5').update(s).digest('hex');
-  return token;
-}
-
-sendEmail = function(email_list){
-  var mailOptions = {  
-  from: 'cmufeedbackr@gmail.com', // sender address
-  to: 'suvrathpen@gmail.com', // list of receivers
-  subject: 'Hello', // Subject line
-  text: 'Hello world ?', // plaintext body
-  html: '<b>Hello world ?</b>' // html body
-};
-console.log(generateToken("suvrathpen@gmail.com"));
-transporter.sendMail(mailOptions, function (error, info) {
-  //Email not sent
-  if (error) {
-      console.log(error);
-  }
-  //Yay!! Email sent
-  else {
-      console.log("email successfully sent" + info.response);
-  }
-});
 
 }
 
