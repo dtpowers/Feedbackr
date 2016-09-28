67afa00049b5b5ed5ac983c5340aa57736ef274b
donovan powers <donovan powers>
2016-09-28 11:47:34 -0400

    changed email message to link correctly

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -5,30 +5,30 @@ var mongoModel = require('../models/mongoModel.js')
 var Baby = require('babyparse');
 
 var transporter = nodemailer.createTransport(smtpTransport({
-    service: 'gmail',
-    auth: {
-      user: 'cmufeedbackr@gmail.com',
-      pass: 'cmufeedbackr1'
-    }
-  }));
+  service: 'gmail',
+  auth: {
+    user: 'cmufeedbackr@gmail.com',
+    pass: 'cmufeedbackr1'
+  }
+}));
 
 exports.init = function(app) {
   app.post('/email', setEmail);
 }
 
 
-generateToken = function(s){
+generateToken = function(s) {
   token = crypto.createHash('md5').update(s).digest('hex');
   return token;
 }
 
 
-handleFiles = function(file){
+handleFiles = function(file) {
   console.log("reached this function");
   console.log(file);
 }
 
-setEmail = function(req, res){
+setEmail = function(req, res) {
   var data = req.body;
   var email = data.email;
   sendEmail([email]);
@@ -37,28 +37,30 @@ setEmail = function(req, res){
 
 //var email_list = ["suvrathpen@gmail.com", "thorasgardthunder@gmail.com"]
 
-sendEmail = function(email_list){
-  for(i = 0; i < email_list.length; i++){
+sendEmail = function(email_list) {
+  for (i = 0; i < email_list.length; i++) {
     token = generateToken(email_list[i]);
     console.log(email_list[i] + 'is: ' + token);
-    var mailOptions = {  
+    var mailOptions = {
       from: 'cmufeedbackr@gmail.com', // sender address
       to: email_list[i], // list of receivers
       subject: 'Hello from FeedbackR', // Subject line
       text: 'Hello from FeedbackR', // plaintext body
-      html: '<p>Hello User. This is your token to log on to the site:' + token + '</p>' 
-             + '<br>' + '<p>Please <a href="http://localhost:3000/studboard/">click here</a> to access student dashboard</p>'// html body
+      html: '<p>Hello User. This is your token to log on to the site:' + token + '</p>' +
+        '<br>' + '<p>Please <a href="https://feedbackr475.herokuapp.com/stuboard">click here</a> to access student dashboard</p>' // html body
     };
-    mongoModel.create('tokens', {'token': token, 'email': email_list[i].email}, 
-    function(error, info){
-      if (error) {
-        console.log(error);
-      }
-      else {
-        console.log("collection successfully created" + info.response);
-      }
-    });
-    transporter.sendMail(mailOptions, function (error, info) {
+    mongoModel.create('tokens', {
+        'token': token,
+        'email': email_list[i].email
+      },
+      function(error, info) {
+        if (error) {
+          console.log(error);
+        } else {
+          console.log("collection successfully created" + info.response);
+        }
+      });
+    transporter.sendMail(mailOptions, function(error, info) {
       //Email not sent
       if (error) {
         console.log(error);
